da3dffc518f292b7445d9bc1d7af3e1f9a692a0d
Troy Klein <spidersddd@users.noreply.github.com>
2016-07-27 09:21:04 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,6 +47,6 @@ node default {
   exec { 'motd 7.2':
     path    => '/usr/local/bin',
     command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
-    unless  => "/bin/test `/bin/grep 'Welcome to ${::fqdn}!' /etc/motd`", 
+    unless  => "/bin/test `/bin/grep 'Welcome to spidersddd.puppetlabs.vm!' /etc/motd`", 
   }
 }
