5bfa2a386469d947b24682be8866361ec1bcd326
Troy Klein <spidersddd@users.noreply.github.com>
2016-07-28 13:43:46 -0500

    Case statment

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,15 +1,43 @@
 class nginx {
 
-  $pkg = 'nginx'
-  $etc_dir = '/etc/nginx'
-  $fowner = 'root'
-  $fgroup = 'root'
-  $docroot = '/var/www'
-  $server_block = "${etc_dir}/conf.d"
-  $log_dir = '/var/log/nginx'
-  $service = 'nginx'
-  $run_as  = 'nginx'
-  
+  case $::osfamily {
+    'RedHat': {
+      $pkg = 'nginx'
+      $etc_dir = '/etc/nginx'
+      $fowner = 'root'
+      $fgroup = 'root'
+      $docroot = '/var/www'
+      $server_block = "${etc_dir}/conf.d"
+      $log_dir = '/var/log/nginx'
+      $service = 'nginx'
+      $run_as  = 'nginx'
+    }
+    'Debian': {
+      $pkg = 'nginx'
+      $etc_dir = '/etc/nginx'
+      $fowner = 'root'
+      $fgroup = 'root'
+      $docroot = '/var/www'
+      $server_block = "${etc_dir}/conf.d"
+      $log_dir = '/var/log/nginx'
+      $service = 'nginx'
+      $run_as  = 'www-data'
+    }
+    'windows': {
+      $pkg = 'nginx-service'
+      $etc_dir = 'C:/ProgramData/nginx'
+      $fowner = 'Administrator'
+      $fgroup = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $server_block = "${etc_dir}/conf.d"
+      $log_dir = 'C:/ProgramData/gninx/logs'
+      $service = 'nginx'
+      $run_as  = 'nobody'
+    }
+    default: {
+      fail("Operating system #{operatingsystem} is not supported.")
+    }
+  }
   
   File {
     owner => $fowner,
