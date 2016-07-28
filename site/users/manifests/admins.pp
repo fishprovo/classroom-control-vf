a03b05b1af804a2e6bdba02bd142149eb78781cd
Troy Klein <spidersddd@users.noreply.github.com>
2016-07-28 11:24:20 -0500

    15.1

---
+++ site/users/manifests/admins.pp
@@ -0,0 +1,10 @@
+class users::admins {
+  users::managed_user { 'jose': }
+  users::managed_user { 'alice': }
+  users::managed_user { 'chen:
+    group => 'admin',
+  }
+  group { 'admin':
+    ensure => present,
+  }
+}
